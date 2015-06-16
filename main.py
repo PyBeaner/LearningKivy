d39f1e52ebc32edb7105b95a591eb711389af710
XiaomingSu <xiaoming.su@patpat.com>
2015-06-16 14:07:06 +0800

    kivy app

---
+++ main.py
@@ -0,0 +1,15 @@
+__author__ = 'PyBeaner'
+
+import kivy
+kivy.require("1.9.0")
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class MyApp(App):
+    def build(self):
+        return Label(text="Hello World!")
+
+if __name__ == "__main__":
+    MyApp().run()
