292f51cf6c3547831f186eca3532d1cedf8e66f9
XiaomingSu <xiaoming.su@patpat.com>
2015-06-18 13:35:34 +0800

    clear touched point

--- Examples/Guide/firstwidget/3_draw_ellipse.py
+++ Examples/Guide/firstwidget/3_draw_ellipse.py
@@ -6,11 +6,24 @@ from kivy.graphics import Color, Ellipse
 
 
 class MyPaintWidget(Widget):
+    touch_point_color = (1, 1, 0)
+    touch_point_radius = 10.0
+    previous_touch_point = None
+
     def on_touch_down(self, touch):
         with self.canvas:
-            Color(1, 1, 0)  #
-            d = 30.0
-            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            Color(*self.touch_point_color)  #
+            Ellipse(pos=(touch.x - self.touch_point_radius, touch.y - self.touch_point_radius),
+                    size=(self.touch_point_radius * 2, self.touch_point_radius * 2))
+            self.previous_touch_point = touch.pos
+
+    def on_touch_up(self, touch):
+        with self.canvas:
+            if self.previous_touch_point:
+                Color(0, 0, 0)  #
+                Ellipse(pos=(self.previous_touch_point[0] - self.touch_point_radius,
+                             self.previous_touch_point[1] - self.touch_point_radius),
+                        size=(self.touch_point_radius * 2, self.touch_point_radius * 2))
 
 
 class MyPaintApp(App):
